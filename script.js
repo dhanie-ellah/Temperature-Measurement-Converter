c85fb2c521b2a473d512bdb751b5e40f6ca4005e
dhanie-ellah <101949297+dhanie-ellah@users.noreply.github.com>
2022-08-25 12:38:41 -0700

    celcius to kelvin error fixed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function converttemp() {
      let from = document.getElementById("from").value;
      let to = document.getElementById("to").value;
-     let constnumber = document.getElementById("number").value;
+     let constnumber = parseInt(document.getElementById("number").value);
 
      if (constnumber) {
           // FROM KELVIN TO FARENHEIT
@@ -21,14 +21,14 @@ function converttemp() {
           else if (from == 'farenheit' && to == 'celcius') {
                let result = (constnumber - 32) * (5 / 9)
                output.innerHTML = `${constnumber} farenheit = ${result.toFixed(2)} celcius`
-          } //KELVIN TO CELCIUS
-          else if (from == 'kelvin' && to == 'celcius') {
-               let result = constnumber - 273.15
-               output.innerHTML = `${constnumber} kelvin = ${result.toFixed(2)} celcius`
           } //CELCIUS TO KELVIN
           else if (from == 'celcius' && to == 'kelvin') {
                let result = constnumber + 273.15
                output.innerHTML = `${constnumber} celcius = ${result.toFixed(2)} kelvin`
+          } //KELVIN TO CELCIUS
+          else if (from == 'kelvin' && to == 'celcius') {
+               let result = constnumber - 273.15
+               output.innerHTML = `${constnumber} kelvin = ${result.toFixed(2)} celcius`
           } else {
                output.innerHTML = 'Select different units of measurement'
           }
